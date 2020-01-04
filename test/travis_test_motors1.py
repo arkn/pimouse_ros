f1b22dd96896cea96df780b0d552b2b9a83d9cc0
arkn <ken.arai.jp@gmail.com>
2020-01-04 12:53:04 +0000

    fixed

--- test/travis_test_motors1.py
+++ test/travis_test_motors1.py
@@ -9,9 +9,6 @@ from geometry_msgs.msg import Twist
 class MotorTest(unittest.TestCase):
     def file_check(self,dev,value,message):
         with open("/dev/"+dev,"r") as f:
-            print("dev = " , dev)
-            print("value = " , str(dev))
-            print("message = " , dev)
             self.assertEqual(f.readline(), str(value)+"\n", message)
 
     def test_node_exist(self):
@@ -27,8 +24,8 @@ class MotorTest(unittest.TestCase):
             pub.publish(m)
             time.sleep(0.1)
 
-        self.file_check("rtmotor_raw_l0", m.left_hz, "wrong left value from motor_raw")
         self.file_check("rtmotor_raw_r0", m.right_hz, "wrong right value from motor_raw")
+        self.file_check("rtmotor_raw_l0", m.left_hz, "wrong left value from motor_raw")
 
     def test_put_cmd_vel(self):
         pub = rospy.Publisher('/cmd_vel', Twist)
@@ -43,8 +40,9 @@ class MotorTest(unittest.TestCase):
         self.file_check("rtmotor_raw_r0", 600, "wrong right value from cmd_vel")
 
         time.sleep(1.2)
-        self.file_check("rtmotor_raw_l0", 0, "don't stop after 1[sec]")
+
         self.file_check("rtmotor_raw_r0", 0, "don't stop after 1[sec]")
+        self.file_check("rtmotor_raw_l0", 0, "don't stop after 1[sec]")
 
 if __name__ == '__main__':
     time.sleep(3)
