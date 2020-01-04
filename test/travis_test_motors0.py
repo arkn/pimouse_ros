8c30cac17ab76093eac5bf1fd386ec80e4dadd9d
arkn <ken.arai.jp@gmail.com>
2020-01-04 18:01:47 +0000

    test

--- test/travis_test_motors0.py
+++ test/travis_test_motors0.py
@@ -61,7 +61,7 @@ class MotorTest(unittest.TestCase):
         self.assertEqual(ret.message, "OFF", "motor off has wrong message")
         with open("/dev/rtmotoren0","r") as f:
             data = f.readline()
-            self.assertEqual(data, "0\n", "wrong value in rtmotor0 at motor off")
+            self.assertEqual(data, "0\n", "wrong value in rtmotoren0 at motor off")
 
         on = rospy.ServiceProxy('/motor_on', Trigger)
         ret = on()
@@ -69,7 +69,7 @@ class MotorTest(unittest.TestCase):
         self.assertEqual(ret.message, "ON", "motor on has wrong message")
         with open("/dev/rtmotoren0", "r") as f:
             data = f.readline()
-            self.assertEqual(data, "1\n", "wrong value in rtmotor0 at motor on")
+            self.assertEqual(data, "1\n", "wrong value in rtmotoren0 at motor on")
 
     def test_put_value_timed(self):
         tm = rospy.ServiceProxy('/timed_motion', TimedMotion)
